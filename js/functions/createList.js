48a344c520aeb46e8a8a81bac5bd164f9a47a2c9
AmaryllisNO <amaryllisno@gmail.com>
2020-09-27 11:57:31 +0200

    seperated modules

---
+++ js/functions/createList.js
@@ -0,0 +1,29 @@
+import { listContainer } from "../constants/constants.js";
+import { removeBook } from "./removeBook.js";
+import { books } from "../utils/books.js";
+
+export function createList() {
+    listContainer.innerHTML = "";
+
+    books.forEach(function (book) {
+
+        let bookTitle = book.title;
+        listContainer.innerHTML += `
+                            <li>
+                                <span>${bookTitle}</span>
+                                <button class="del-book-btn"><i class="fas fa-trash-alt" data-title="${bookTitle}"></i></button>
+                            </li>`
+
+    })
+
+    const button = document.querySelectorAll(".del-book-btn");
+
+    button.forEach((button) => {
+        button.addEventListener("click", removeBook);
+    })
+
+    if (books.length === 0) {
+        listContainer.innerHTML += '<div class="error-message">Add some books to the list.</div>';
+    }
+
+}
--- js/script.js
+++ js/script.js
@@ -1,38 +1,10 @@
-import { listContainer, input } from "./constants/constants.js";
-
-let books = [];
-
-
-
-function createList() {
-    listContainer.innerHTML = "";
+import { input } from "./constants/constants.js";
+import { createList } from "./functions/createList.js"
+import { books } from "./utils/books.js";
 
 
-    books.forEach(function (book) {
 
-        let bookTitle = book.title;
-        listContainer.innerHTML += `
-                            <li>
-                                <span>${bookTitle}</span>
-                                <button class="del-book-btn"><i class="fas fa-trash-alt" data-title="${bookTitle}"></i></button>
-                            </li>`
-
-    })
-
-    const button = document.querySelectorAll(".del-book-btn");
-
-    button.forEach((button) => {
-        button.addEventListener("click", removeBook);
-    })
-
-    if (books.length === 0) {
-        listContainer.innerHTML += '<div class="error-message">Whoops, no books are in the list!</div>';
-    }
-}
-
-button.addEventListener("click", addItem);
-
-function addItem() {
+export function addItem() {
     const itemValue = input.value.trim();
     console.log(itemValue);
 
@@ -40,28 +12,11 @@ function addItem() {
         const newItem = { title: itemValue };
         //  console.log(input.value); 
         books.push(newItem);
-        // console.log(todos); 
 
         createList(books);
     }
 }
 
-
-function removeBook() {
-    const deleteThisBook = event.target.dataset.title;
-
-    let newBooks = books.filter(function (book) {
-        if (deleteThisBook !== book.title) {
-            return true;
-        }
-    })
-
-    console.log(newBooks);
-
-    books = newBooks;
-
-    createList();
-}
-
 createList();
 
+button.addEventListener("click", addItem);
